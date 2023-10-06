fe99f5b40fe4139ab740f40cc1fd42d5b80fb5a1
perov_dev <mihailperov90@gmail.com>
2023-10-06 17:49:36 +0300

    Add post, test and user directory

---
+++ src/post/Forget.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Forget = () => {
+    return <>
+        <p>forget to add this in commit...</p>
+    </>
+}
+
+export default Forget
---
+++ src/post/Post.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Post = () => {
+    return <>
+        <h3>Post</h3>
+        <p>forget to add this in commit...</p>
+    </>
+}
+
+export default Post
